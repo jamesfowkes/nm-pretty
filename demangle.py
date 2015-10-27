e5a2008e1a8e8b6549b5633962b7b3f22adae5b3
James Fowkes <jamesfowkes@gmail.com>
2015-10-27 14:54:08 +0000

    Removed demangler

--- demangle.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import locale
-
-def demangle_cpp_name(string):
-	output = subprocess.check_output(["c++filt", string])
-	output = output.decode(locale.getpreferredencoding(False)).strip()
-	return output
