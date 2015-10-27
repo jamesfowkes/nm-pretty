a37cb57deaf3aa177f8976a9ce733743267bba36
James Fowkes <jamesfowkes@gmail.com>
2015-10-27 14:18:39 +0000

    Added nm type descriptions

---
+++ nm.py
@@ -0,0 +1,35 @@
+__descriptions = {
+	'B': "Uninitialized data (global)",
+	'b': "Uninitialized data",
+	'D': "Initialized data (global)",
+	'd': "Initialized data",
+	'G': "Initialized small objects (global)",
+	'g': "Initialized small objects",
+	'i': "Indirect functions",
+	'I': "Indirect references",
+	'N': "Debugging symbols",
+	'P': "Stack unwind symbols",
+	'R': "Read-only data (global)",
+	'r': "Read-only data",
+	'S': "Unintialized small objects (global)",
+	's': "Uninitialized small objects",
+	'T': "Code (global)",
+	't': "Code",
+	'U': "Undefined",
+	'u': "Unique global symbols",
+	'V': "Weak objects",
+	'v': "Weak objects",
+	'W': "Weak symbols",
+	'w': "Weak symbols",
+	'-': "stabs symbols",
+	'?': "Unknown symbols"
+}
+
+def get_types():
+	return __descriptions.keys()
+
+def get_description_for_type(type_letter):
+	try:
+		return __descriptions[type_letter]
+	except:
+		return "Unknown symbol"
